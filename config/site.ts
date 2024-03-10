3b5b163a39f2ed17b127ebaed1718bb10ee7471c
ndyabagye <ndyabagyehenrytusi@gmail.com>
2024-03-10 17:42:42 +0300

    Modify Site Config to include deployed url

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
   name: "HenryBlog",
-  url: "https://example.com",
+  url: "https://nextjs-velite-blog.vercel.app",
   description: "Nextjs 14 blog using Velite, Tailwind, Shadcn UI and MDX",
   author: "Ndyabagye Henry",
   links: {
     twitter: "https://twitter.com/henry_ndyabagye",
     github: "https://github.com/ndyabagye",
-    personalSite: "",
+    personalSite: "https://nextjs-velite-blog.vercel.app/",
   },
 };
 
